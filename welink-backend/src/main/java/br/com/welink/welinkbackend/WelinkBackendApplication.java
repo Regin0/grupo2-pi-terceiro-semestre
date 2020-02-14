4d486085818eabf5b3b2865149b17756131c4b14
WilliamMN <w1lliam.m4rques@gmail.com>
2020-02-14 10:42:05 -0300

    spring boot backend

---
+++ welink-backend/src/main/java/br/com/welink/welinkbackend/WelinkBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.welink.welinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelinkBackendApplication.class, args);
+	}
+
+}
---
+++ welink-backend/src/test/java/br/com/welink/welinkbackend/WelinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.welink.welinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
